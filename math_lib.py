9dacde1db2ad62debf702745ab300ff2f3b9fb7f
Kacperch1 <111431914+Kacperch1@users.noreply.github.com>
2023-07-11 17:38:55 +0200

    Add files via upload

---
+++ math_lib.py
@@ -0,0 +1,24 @@
+def add(a: int, b: int):
+    return a + b
+print(add(55, 212))
+
+267
+
+def sub(a: int, b: int):
+    return a - b
+print(sub(652, 237))
+
+def multi(x: int, y):
+    return x * y
+print(multi(772, 321))
+
+def div(x: int, y: int):
+    return x / y
+print(div(8823, 3))
+
+## homework with power
+def pow2(x: int, y: int):
+    return x**y
+
+print(pow2(2, 6))
+print(pow2(64, 8))
